a4675c0a1e3e5cbfd8f98ce1b7ed6dac56ff9e35
d4phan <d4phan@ucsd.edu>
2025-10-18 19:17:14 -0700

    Adjusted JS

--- global.js
+++ global.js
@@ -1,13 +1,43 @@
 console.log('IT’S ALIVE!');
 
 function $$(selector, context = document) {
-  return Array.from(context.querySelectorAll(selector));
+    return Array.from(context.querySelectorAll(selector));
 }
 
-let navLinks = $$("nav a");
+let pages = [
+    { url: '', title: 'Home' },
+    { url: 'projects/', title: 'Projects' },
+    { url: 'resume/', title: 'CV' },
+    { url: 'contact/', title: 'Contact' },
+    { url: 'https://github.com/d4phan', title: 'GitHub' }
+];
 
-let currentLink = navLinks.find(
-  (a) => a.host === location.host && a.pathname === location.pathname
-);
+const BASE_PATH = (location.hostname === "localhost" || location.hostname === "127.0.0.1")
+  ? "/"
+  : "/portfolio/"; 
 
-currentLink?.classList.add('current');
+let nav = document.createElement('nav');
+document.body.prepend(nav);
+
+for (let p of pages) {
+    let url = p.url;
+    let title = p.title;
+  
+    if (!url.startsWith('http')) {
+        url = BASE_PATH + url;
+    }
+  
+    let a = document.createElement('a');
+    a.href = url;
+    a.textContent = title;
+  
+    if (a.host === location.host && a.pathname === location.pathname) {
+        a.classList.add('current');
+    }
+  
+    if (a.host !== location.host) {
+        a.target = "_blank";
+    }
+  
+    nav.append(a);
+}
