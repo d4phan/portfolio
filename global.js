a9b8fa4379862fb4023b4985db4188d331755c09
d4phan <d4phan@ucsd.edu>
2025-10-24 21:36:40 -0700

    Updates

--- global.js
+++ global.js
@@ -109,3 +109,7 @@ export function renderProjects(projects, containerElement, headingLevel = 'h2') 
 		containerElement.append(article);
 	}
 }
+
+export async function fetchGitHubData(username) {
+	return fetchJSON(`https://api.github.com/users/${username}`);
+}
