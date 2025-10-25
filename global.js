a8200616c6978d3e044badbe1fda13adc2e39786
d4phan <d4phan@ucsd.edu>
2025-10-24 18:53:56 -0700

    Revert "Include projects.json"
    This reverts commit 9142c26ded0eb89893fe09a8581893f288ea9e84.

--- global.js
+++ global.js
@@ -1,116 +1,75 @@
 console.log('IT’S ALIVE!');
 
 function $$(selector, context = document) {
-	return Array.from(context.querySelectorAll(selector));
+    return Array.from(context.querySelectorAll(selector));
 }
 
 let pages = [
-	{ url: '', title: 'Home' },
-	{ url: 'projects/', title: 'Projects' },
-	{ url: 'resume/', title: 'CV' },
-	{ url: 'contact/', title: 'Contact' },
-	{ url: 'https://github.com/d4phan', title: 'GitHub' }
+    { url: '', title: 'Home' },
+    { url: 'projects/', title: 'Projects' },
+    { url: 'resume/', title: 'CV' },
+    { url: 'contact/', title: 'Contact' },
+    { url: 'https://github.com/d4phan', title: 'GitHub' }
 ];
 
 const BASE_PATH = (location.hostname === "localhost" || location.hostname === "127.0.0.1")
-	? "/"
-	: "/portfolio/";
+    ? "/"
+    : "/portfolio/";
 
 let nav = document.createElement('nav');
 document.body.prepend(nav);
 
 for (let p of pages) {
-	let url = p.url;
-	let title = p.title;
-
-	if (!url.startsWith('http')) {
-		url = BASE_PATH + url;
-	}
-
-	let a = document.createElement('a');
-	a.href = url;
-	a.textContent = title;
-
-	if (a.host === location.host && a.pathname === location.pathname) {
-		a.classList.add('current');
-	}
-
-	if (a.host !== location.host) {
-		a.target = "_blank";
-	}
-
-	nav.append(a);
+    let url = p.url;
+    let title = p.title;
+  
+    if (!url.startsWith('http')) {
+        url = BASE_PATH + url;
+    }
+  
+    let a = document.createElement('a');
+    a.href = url;
+    a.textContent = title;
+  
+    if (a.host === location.host && a.pathname === location.pathname) {
+        a.classList.add('current');
+    }
+  
+    if (a.host !== location.host) {
+        a.target = "_blank";
+    }
+  
+    nav.append(a);
 }
 
 document.body.insertAdjacentHTML(
-	'afterbegin',
-	`
-	<label class="color-scheme">
-		Theme:
-		<select>
-		<option value="light dark">Automatic</option>
-		<option value="light">Light</option>
-		<option value="dark">Dark</option>
-		</select>
-	</label>
-	`
+    'afterbegin',
+    `
+    <label class="color-scheme">
+        Theme:
+        <select>
+        <option value="light dark">Automatic</option>
+        <option value="light">Light</option>
+        <option value="dark">Dark</option>
+        </select>
+    </label>
+    `
 );
 
 const select = document.querySelector('.color-scheme select');
 
 if ("colorScheme" in localStorage) {
-	const savedScheme = localStorage.colorScheme;
-	document.documentElement.style.setProperty('color-scheme', savedScheme);
-	select.value = savedScheme;
+    const savedScheme = localStorage.colorScheme;
+    document.documentElement.style.setProperty('color-scheme', savedScheme);
+    select.value = savedScheme;
 }
 else {
-	document.documentElement.style.setProperty('color-scheme', 'light');
-	select.value = 'light';
+    document.documentElement.style.setProperty('color-scheme', 'light');
+    select.value = 'light';
 }
 
 select.addEventListener('input', function (event) {
-	const newScheme = event.target.value;
-	document.documentElement.style.setProperty('color-scheme', newScheme);
-	localStorage.colorScheme = newScheme;
+    const newScheme = event.target.value;
+    document.documentElement.style.setProperty('color-scheme', newScheme);
+    localStorage.colorScheme = newScheme;
 });
-
-export async function fetchJSON(url) {
-	try {
-		const response = await fetch(url);
-		
-		if (!response.ok) {
-			throw new Error(`Failed to fetch projects: ${response.statusText}`);
-		}
-		
-		const data = await response.json();
-		return data;
-	} catch (error) {
-		console.error('Error fetching or parsing JSON data:', error);
-	}
-}
-
-export function renderProjects(projects, containerElement, headingLevel = 'h2') {
-	if (projects.length === 0) {
-		containerElement.innerHTML = '<p>No projects to display yet.</p>';
-		return;
-	}
-	
-	containerElement.innerHTML = '';
-	
-	for (let project of projects) {
-		const article = document.createElement('article');
-		
-		const heading = document.createElement(headingLevel);
-		heading.textContent = project.title;
-		
-		const img = document.createElement('img');
-		img.src = project.image;
-		img.alt = project.title || '';
-		
-		const description = document.createElement('p');
-		description.textContent = project.description;
-		
-		article.append(heading, img, description);
-		containerElement.append(article);
-	}
-}
