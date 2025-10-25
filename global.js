6b00946cf3401e007d3165488a33be43be7b3ab3
d4phan <d4phan@ucsd.edu>
2025-10-24 19:33:42 -0700

    Update global.js

--- global.js
+++ global.js
@@ -1,77 +1,77 @@
 console.log('IT’S ALIVE!');
 
 function $$(selector, context = document) {
-    return Array.from(context.querySelectorAll(selector));
+	return Array.from(context.querySelectorAll(selector));
 }
 
 let pages = [
-    { url: '', title: 'Home' },
-    { url: 'projects/', title: 'Projects' },
-    { url: 'resume/', title: 'CV' },
-    { url: 'contact/', title: 'Contact' },
-    { url: 'https://github.com/d4phan', title: 'GitHub' }
+	{ url: '', title: 'Home' },
+	{ url: 'projects/', title: 'Projects' },
+	{ url: 'resume/', title: 'CV' },
+	{ url: 'contact/', title: 'Contact' },
+	{ url: 'https://github.com/d4phan', title: 'GitHub' }
 ];
 
 const BASE_PATH = (location.hostname === "localhost" || location.hostname === "127.0.0.1")
-    ? "/"
-    : "/portfolio/";
+	? "/"
+	: "/portfolio/";
 
 let nav = document.createElement('nav');
 document.body.prepend(nav);
 
 for (let p of pages) {
-    let url = p.url;
-    let title = p.title;
-  
-    if (!url.startsWith('http')) {
-        url = BASE_PATH + url;
-    }
-  
-    let a = document.createElement('a');
-    a.href = url;
-    a.textContent = title;
-  
-    if (a.host === location.host && a.pathname === location.pathname) {
-        a.classList.add('current');
-    }
-  
-    if (a.host !== location.host) {
-        a.target = "_blank";
-    }
-  
-    nav.append(a);
+	let url = p.url;
+	let title = p.title;
+
+	if (!url.startsWith('http')) {
+		url = BASE_PATH + url;
+	}
+
+	let a = document.createElement('a');
+	a.href = url;
+	a.textContent = title;
+
+	if (a.host === location.host && a.pathname === location.pathname) {
+		a.classList.add('current');
+	}
+
+	if (a.host !== location.host) {
+		a.target = "_blank";
+	}
+
+	nav.append(a);
 }
 
 document.body.insertAdjacentHTML(
-    'afterbegin',
-    `
-    <label class="color-scheme">
-        Theme:
-        <select>
-        <option value="light dark">Automatic</option>
-        <option value="light">Light</option>
-        <option value="dark">Dark</option>
-        </select>
-    </label>
-    `
+	'afterbegin',
+	`
+	<label class="color-scheme">
+		Theme:
+		<select>
+		<option value="light dark">Automatic</option>
+		<option value="light">Light</option>
+		<option value="dark">Dark</option>
+		</select>
+	</label>
+	`
 );
 
 const select = document.querySelector('.color-scheme select');
 
 if ("colorScheme" in localStorage) {
-    const savedScheme = localStorage.colorScheme;
-    document.documentElement.style.setProperty('color-scheme', savedScheme);
-    select.value = savedScheme;
+	const savedScheme = localStorage.colorScheme;
+	document.documentElement.style.setProperty('color-scheme', savedScheme);
+	select.value = savedScheme;
 }
 else {
-    document.documentElement.style.setProperty('color-scheme', 'light');
-    select.value = 'light';
+	document.documentElement.style.setProperty('color-scheme', 'light');
+	select.value = 'light';
 }
 
 select.addEventListener('input', function (event) {
-    const newScheme = event.target.value;
-    document.documentElement.style.setProperty('color-scheme', newScheme);
-    localStorage.colorScheme = newScheme;
+	const newScheme = event.target.value;
+	document.documentElement.style.setProperty('color-scheme', newScheme);
+	localStorage.colorScheme = newScheme;
 });
 
 export async function fetchJSON(url) {
