2c6c97f7aea370096fb504c23d7aeb60cab76885
d4phan <d4phan@ucsd.edu>
2025-11-08 18:57:26 -0800

    Update main.js

--- meta/main.js
+++ meta/main.js
@@ -145,6 +145,14 @@ function renderScatterPlot(data, commits) {
         .domain([0, 24])
         .range([usableArea.bottom, usableArea.top]);
 
+    const [minLines, maxLines] = d3.extent(commits, d => d.totalLines);
+    const rScale = d3
+        .scaleSqrt()
+        .domain([minLines, maxLines])
+        .range([2, 30]);
+
+    const sortedCommits = d3.sort(commits, d => -d.totalLines);
+
     const gridlines = svg
         .append('g')
         .attr('class', 'gridlines')
@@ -171,20 +179,22 @@ function renderScatterPlot(data, commits) {
 
     const dots = svg.append('g').attr('class', 'dots');
 
-    dots
-        .selectAll('circle')
-        .data(commits)
+    dots.selectAll('circle')
+        .data(sortedCommits)
         .join('circle')
         .attr('cx', d => xScale(d.datetime))
         .attr('cy', d => yScale(d.hourFrac))
-        .attr('r', 5)
+        .attr('r', d => rScale(d.totalLines))
         .attr('fill', 'steelblue')
+        .style('fill-opacity', 0.7)
         .on('mouseenter', (event, commit) => {
+            d3.select(event.currentTarget).style('fill-opacity', 1);
             renderTooltipContent(commit);
             updateTooltipVisibility(true);
             updateTooltipPosition(event);
         })
-        .on('mouseleave', () => {
+        .on('mouseleave', (event) => {
+            d3.select(event.currentTarget).style('fill-opacity', 0.7);
             updateTooltipVisibility(false);
         });
 }
