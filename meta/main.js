291861fddb1950f0c689ebe77d1f6f8b241e0aaf
d4phan <d4phan@ucsd.edu>
2025-11-22 16:26:37 -0800

    update look

--- meta/main.js
+++ meta/main.js
@@ -79,7 +79,7 @@ function updateFileDisplay(filteredCommits) {
     filesContainer.select('dt > small').text((d) => `${d.lines.length} lines`);
 
     filesContainer
-        .select('dd')
+        .selectAll('dd')
         .selectAll('div')
         .data((d) => d.lines)
         .join('div')
@@ -88,43 +88,38 @@ function updateFileDisplay(filteredCommits) {
 }
 
 function renderCommitInfo(data, commits) {
-    const dl = d3.select('#stats').append('dl').attr('class', 'stats');
+    const container = d3.select('#stats');
+    container.selectAll('*').remove();
+    
+    const stats = [
+        { label: 'COMMITS', value: commits.length },
+        { label: 'FILES', value: d3.group(data, d => d.file).size },
+        { label: 'TOTAL LOC', value: data.length },
+        { 
+            label: 'MAX DEPTH', 
+            value: d3.max(data, d => d.depth)
+        },
+        { 
+            label: 'LONGEST LINE', 
+            value: d3.max(data, d => d.length)
+        },
+        { 
+            label: 'MAX LINES', 
+            value: d3.max(
+                d3.rollups(data, v => d3.max(v, d => d.line), d => d.file),
+                d => d[1]
+            )
+        }
+    ];
 
-    dl.append('dt').html('Total <abbr title="Lines of code">LOC</abbr>');
-    dl.append('dd').text(data.length);
+    const statBoxes = container
+        .selectAll('div')
+        .data(stats)
+        .join('div')
+        .attr('class', 'stat-box');
 
-    dl.append('dt').text('Total commits');
-    dl.append('dd').text(commits.length);
-
-    let numFiles = d3.group(data, d => d.file).size;
-    dl.append('dt').text('Number of files');
-    dl.append('dd').text(numFiles);
-
-    const fileLengths = d3.rollups(
-        data,
-        v => d3.max(v, d => d.line),
-        d => d.file
-    );
-    const avgFileLength = d3.mean(fileLengths, d => d[1]);
-    dl.append('dt').text('Average file length');
-    dl.append('dd').text(avgFileLength.toFixed(2) + ' lines');
-
-    const longestFile = d3.greatest(fileLengths, d => d[1]);
-    dl.append('dt').text('Longest file');
-    dl.append('dd').html(`${longestFile[0]} (${longestFile[1]} lines)`);
-
-    let avgLineLength = d3.mean(data, d => d.length);
-    dl.append('dt').text('Average line length');
-    dl.append('dd').text(avgLineLength.toFixed(2) + ' characters');
-
-    const workByPeriod = d3.rollups(
-        commits,
-        v => v.length,
-        d => d.datetime.toLocaleString('en', { dayPeriod: 'short' })
-    );
-    const mostActivePeriod = d3.greatest(workByPeriod, d => d[1]);
-    dl.append('dt').text('Most active period');
-    dl.append('dd').text(`${mostActivePeriod[0]} (${mostActivePeriod[1]} commits)`);
+    statBoxes.append('div').attr('class', 'stat-label').text(d => d.label);
+    statBoxes.append('div').attr('class', 'stat-value').text(d => d.value);
 }
 
 function renderTooltipContent(commit) {
